db44f4c46596f16acf490be832fcebd800ad0e62
Nicole <goolitesum@gmail.com>
2021-07-06 11:04:17 +0800

    Initial Commit

---
+++ homework_02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework_02
+//
+//  Created by Nicole on 6/7/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ homework_02Tests/homework_02Tests.swift
@@ -0,0 +1,33 @@
+//
+//  homework_02Tests.swift
+//  homework_02Tests
+//
+//  Created by Nicole on 6/7/2021.
+//
+
+import XCTest
+@testable import homework_02
+
+class homework_02Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
