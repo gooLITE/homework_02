d72c5858285bd9f930d6bfd755f6f9f0fb0a314f
Nicole <goolitesum@gmail.com>
2021-07-07 18:02:50 +0800

    update

--- homework_02/ViewController.swift
+++ homework_02/ViewController.swift
@@ -11,18 +11,43 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet weak var tableView: UITableView!
     let myData = ["first", "second", "third"]
-    
+    let movieURL = "http://trailers.apple.com/trailers/home/feeds/just_added.json"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        //for navigationItems
+        title = "Movie Trailers"
+        configureItems()
         
+        //for tableView
         let nib = UINib(nibName: "MovieListTableViewCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "MovieListTableViewCell")
         tableView.delegate = self
         tableView.dataSource = self
+        
+        //for URLSession
+        let url = URL(string: "https://trailers.apple.com/trailers/home/feeds/just_added.json")!
+        let task = URLSession.shared.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+            if let error = error{
+                print("Error: ", error)
+                return
+            }
+            
+            //let json = try! JSONSerialization.jsonObject(with: data!, options: []) as! Dictionary<String, AnyObject>
+            //print(json)
+            
+            
+            
+            
+        }
+        task.resume()
     }
     
+    //Navigation stuffs
+    private func configureItems(){
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: nil)
+    }
     
     // TableView Functons
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {             //set no. of rows of the table
@@ -35,8 +60,47 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
-    func tableView(_ tableView: UITableView,titleForHeaderInSection section: Int) -> String? {
+    /*func tableView(_ tableView: UITableView,titleForHeaderInSection section: Int) -> String? {
+        
         return "Movie Trailers"
+    }
+    */
+    
+    /*func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let titleLable : UILabel = UILabel()
+        if section == 0 {
+            titleLable.text = " Movie Trailers"
+            titleLable.font = UIFont.boldSystemFont(ofSize: 30)
+            
+            
+        }
+        return titleLable
+    }
+    */
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //println
+    }
+    
+    func performRequest(urlString : String){
+        if let url = URL(string: urlString){
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            task.resume()
+        }
+        
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?){
+        if error != nil{
+            print(error!)
+            return
+        }
+        
+        if let safeData = data{
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString!)
+        }
+        
     }
 }
 
